9b1ff96d913818939660f80d5d2664e4926308dd
Purpzie <25022704+Purpzie@users.noreply.github.com>
2022-06-11 17:09:03 -0500

    chore: make CI fail on broken doc link

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![forbid(unsafe_code)]
+#![deny(rustdoc::broken_intra_doc_links)]
 #![warn(missing_docs)]
 #![allow(clippy::tabs_in_doc_comments)]
 #![cfg_attr(not(feature = "std"), no_std)]
